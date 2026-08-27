17ff80648a090d982eda4e0a8bffe9f7d552dbd0
DoraYaroshenko <yaroshenko@mail.tau.ac.il>
2026-08-27 12:54:03 +0000

    fixed logger

--- UserCommon/src/Logger.cpp
+++ UserCommon/src/Logger.cpp
@@ -7,12 +7,10 @@ using namespace common;
 
 
 
-std::ofstream Logger::out_stream_;
-std::size_t Logger::current_step_id_ = 0;
-std::mutex Logger::mutex_;
+thread_local std::ofstream Logger::out_stream_;
+thread_local std::size_t Logger::current_step_id_ = 0;
 
 void Logger::init(const std::string& path) {
-    std::lock_guard<std::mutex> lock(mutex_);
     if (out_stream_.is_open()) {
         out_stream_.close();
     }
@@ -24,21 +22,18 @@ void Logger::init(const std::string& path) {
 }
 
 void Logger::close() {
-    std::lock_guard<std::mutex> lock(mutex_);
     if (out_stream_.is_open()) {
         out_stream_.close();
     }
 }
 
 void Logger::setStep(std::size_t step_id) {
-    std::lock_guard<std::mutex> lock(mutex_);
     current_step_id_ = step_id;
 }
 
 void Logger::logMovement(double x_cm, double y_cm, double z_cm, 
                          double h_angle_deg, double v_angle_deg, 
                          const std::string& action) {
-    std::lock_guard<std::mutex> lock(mutex_);
     if (!out_stream_.is_open()) return;
 
     out_stream_ << "{\"type\": \"movement\", \"step_id\": " << current_step_id_ 
@@ -48,7 +43,6 @@ void Logger::logMovement(double x_cm, double y_cm, double z_cm,
 }
 
 void Logger::logScan(double relative_h_angle_deg, double relative_v_angle_deg) {
-    std::lock_guard<std::mutex> lock(mutex_);
     if (!out_stream_.is_open()) return;
 
     out_stream_ << "{\"type\": \"scan\", \"step_id\": " << current_step_id_ 
@@ -57,7 +51,6 @@ void Logger::logScan(double relative_h_angle_deg, double relative_v_angle_deg) {
 }
 
 void Logger::logVoxel(int x, int y, int z, int value) {
-    std::lock_guard<std::mutex> lock(mutex_);
     if (!out_stream_.is_open()) return;
 
     out_stream_ << "{\"type\": \"voxel\", \"step_id\": " << current_step_id_ 
